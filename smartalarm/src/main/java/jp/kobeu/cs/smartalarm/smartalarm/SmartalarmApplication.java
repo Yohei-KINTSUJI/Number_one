0cabaa58e29b61d56f75a72fc059fd4d84c00a96
Yohei-KINTSUJI <230t227t@gsuite.kobe-u.ac.jp>
2023-07-24 09:52:59 +0900

    SmartAlarmひな形作成

---
+++ smartalarm/src/main/java/jp/kobeu/cs/smartalarm/smartalarm/SmartalarmApplication.java
@@ -0,0 +1,13 @@
+package jp.kobeu.cs.smartalarm.smartalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartalarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartalarmApplication.class, args);
+	}
+
+}
